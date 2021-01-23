0bde43efdb0636780b1ae306fb75e3f2ae196334
AbhaySingh5349 <abhay5349singh@gmail.com>
2021-01-23 23:27:56 +0530

    updating layout

--- app/src/main/java/com/example/facedetector/MainActivity.java
+++ app/src/main/java/com/example/facedetector/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.camTypeSwitch)
+    SwitchMaterial camTypeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
